84f86364fa7cbd44cf3b0d5f4fae72371490635b
emmakuen <emmaku@protonmail.com>
2022-05-23 17:11:05 +0900

    create registration functionality

--- discord-backend/controllers/auth/postRegister.js
+++ discord-backend/controllers/auth/postRegister.js
@@ -1,5 +1,39 @@
+const User = require("../../models/User");
+const bcrypt = require("bcryptjs");
+
 const postRegister = async (req, res) => {
-  res.send("register route");
+  try {
+    const { username, password, email } = req.body;
+
+    const emailLower = email.toLowerCase();
+    // check if user exists
+    const userExists = await User.exists({ email: emailLower });
+    if (userExists) {
+      return res.status(409).send("E-mail already in use."); // conflict response
+    }
+
+    // encrypt password
+    const encryptedPassword = await bcrypt.hash(password, 10);
+
+    // create user doc and save in database
+    const user = await User.create({
+      username,
+      email: emailLower,
+      password: encryptedPassword,
+    });
+
+    // create JWT token
+    const token = "JWT TOKEN";
+    res.status(201).json({
+      userDetails: {
+        email: user.email,
+        token,
+        username: user.username,
+      },
+    });
+  } catch (err) {
+    return res.status(500).send("Error occured. Please try again.");
+  }
 };
 
 module.exports = postRegister;
