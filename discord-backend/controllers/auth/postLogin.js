81228cb707f4230a3bb5afda5c3fdc451929a860
emmakuen <emmaku@protonmail.com>
2022-05-23 17:28:19 +0900

    create login functionality

--- discord-backend/controllers/auth/postLogin.js
+++ discord-backend/controllers/auth/postLogin.js
@@ -1,5 +1,29 @@
+const User = require("../../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 const postLogin = async (req, res) => {
-  res.send("login route");
+  try {
+    const { email, password } = req.body;
+    const emailLower = email.toLowerCase();
+    const user = await User.findOne({ email: emailLower });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      // send new token
+      const token = "JWT_TOKEN";
+      return res.status(200).json({
+        userDetails: {
+          email: user.email,
+          token,
+          username: user.username,
+        },
+      });
+    }
+
+    return res.status(400).send("Invalid credentials. Please try again."); // 400 - bad request
+  } catch (err) {
+    return res.status(500).send("Something went wrong. Please try again.");
+  }
 };
 
 module.exports = postLogin;
