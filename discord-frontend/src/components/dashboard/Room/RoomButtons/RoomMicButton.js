662c6ad377ff54ada0a3f55bef3b94480cb86c7d
emmakuen <emmaku@protonmail.com>
2022-06-29 16:16:59 +0900

    make mute button functional

--- discord-frontend/src/components/dashboard/Room/RoomButtons/RoomMicButton.js
+++ discord-frontend/src/components/dashboard/Room/RoomButtons/RoomMicButton.js
@@ -2,9 +2,12 @@ import React from "react";
 import { IconButton } from "@mui/material";
 import { Mic, MicOff } from "@mui/icons-material";
 
-const RoomMicButton = ({ iconStyle }) => {
+const RoomMicButton = ({ iconStyle, localStream }) => {
   const [isMicEnabled, setIsMicEnabled] = React.useState(true);
-  const toggleMic = () => setIsMicEnabled(!isMicEnabled);
+  const toggleMic = () => {
+    localStream.getAudioTracks()[0].enabled = !isMicEnabled;
+    setIsMicEnabled(!isMicEnabled);
+  };
   return (
     <IconButton onClick={toggleMic}>
       {isMicEnabled ? <Mic style={iconStyle} /> : <MicOff style={iconStyle} />}
